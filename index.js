a314c350f758b6af5ad91ca55cccadcde11f8ef5
Saklain Mustak <saklainmustak@sakmac.local>
2022-07-29 17:49:32 +0600

    new API loaded

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!!')
+})
+app.get('/new',(req, res)=>{
+  res.send('Another page ...')
 })
 
 app.listen(port, () => {
