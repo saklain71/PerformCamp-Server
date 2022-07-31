a06949759272890e7b08ebf33ca068dfd006f745
Saklain Mustak <saklainmustak@sakmac.local>
2022-08-01 00:57:53 +0600

    database connected

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+// performcamp
+// password -  7LDJdbsGEc8JChAz
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://performcamp:7LDJdbsGEc8JChAzS@cluster0.h1do2.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("performcamp").collection("data");
+  console.log("Db Connected");
+  // perform actions on the collection object
+  client.close();
+});
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!!')
 })
